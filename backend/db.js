93a6db5dc2f3e2c3a7ce31aaf2ba202b07ae8670
Aryan Sindhi <37263346+aryansindhi18@users.noreply.github.com>
2024-02-22 01:53:25 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const {mongoString} = require("./config.js")
+// const {mongoString} = require("./config.js")
 mongoose.connect('mongodb+srv://aryansindhi18:wsxokN0657%40@aryansindhi18.orx7din.mongodb.net/todoapp');
 
 const toDoSchema = new mongoose.Schema({
