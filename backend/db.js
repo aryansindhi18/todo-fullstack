c620749c4945014dbb6a5c62dc447dc01450c0ae
Aryan Sindhi <37263346+aryansindhi18@users.noreply.github.com>
2024-02-22 01:25:58 +0530

    Update db.js
    update mongoString for deployment

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const {mongoString} = require("./config.js")
-mongoose.connect(mongoString);
+mongoose.connect('mongodb+srv://aryansindhi18:wsxokN0657%40@aryansindhi18.orx7din.mongodb.net/todoapp');
 
 const toDoSchema = new mongoose.Schema({
     title: String, // String is shorthand for {type: String}
