a246ed319a22aba3dbdb49a23f35d01f4adb9810
aryansindhi18 <aryansindhi18@gmail.com>
2024-02-22 23:10:01 +0530

    added env variables

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 // const {mongoString} = require("./config.js")
-mongoose.connect('mongodb+srv://aryansindhi18:wsxokN0657%40@aryansindhi18.orx7din.mongodb.net/todoapp');
+mongoose.connect(process.env.MONGOURL);
 
 const toDoSchema = new mongoose.Schema({
     title: String, // String is shorthand for {type: String}
