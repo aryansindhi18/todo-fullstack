5aeee3fe4a0d5971af8991d62d675f634007869a
aryansindhi18 <aryansindhi18@gmail.com>
2024-02-24 13:15:41 +0530

    backend down issue

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
-const swaggerJSDocs = YAML.load("api.yaml");
+const swaggerJSDocs = YAML.load("./api.yaml");
 
 const options = {
     // customCss: ` img {content:url(\'../logo.svg\'); height:auto;} `,
