f93752d300e88889224c7fbc311406ce95482671
aryansindhi18 <aryansindhi18@gmail.com>
2024-02-24 15:59:20 +0530

    added custom css to fix swagger ui

--- backend/config.js
+++ backend/config.js
@@ -4,8 +4,7 @@ const YAML = require("yamljs");
 const swaggerJSDocs = YAML.load(path.join(__dirname, 'api.yaml'));
 
 const options = {
-    customCss: `https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css`,
-    // customfavIcon: "../favicon.ico",
+    customCss: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
     customSiteTitle: "API docs - ToDo ~AryanSindhi"
 }; 
 
