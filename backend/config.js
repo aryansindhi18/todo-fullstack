956727ef27589ca412e1fb5e8675f9315297d521
aryansindhi18 <aryansindhi18@gmail.com>
2024-02-24 13:20:40 +0530

    api.yaml issue

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
-const swaggerJSDocs = YAML.load("./api.yaml");
+const swaggerJSDocs = YAML.load(path.join(__dirname, 'api.yaml'));
 
 const options = {
     // customCss: ` img {content:url(\'../logo.svg\'); height:auto;} `,
